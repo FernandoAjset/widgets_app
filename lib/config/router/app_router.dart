8c372f843780f347a83d8c376813799e058fb85d
eajset <eajset@lucalza.com>
2023-08-22 19:07:42 -0600

    snackbar agregado.

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -25,5 +25,10 @@ final appRouter = GoRouter(
       path: '/progress',
       builder: (context, state) => const ProgressScreen(),
     ),
+            GoRoute(
+      name: SnackBarScreen.name,
+      path: '/snackbars',
+      builder: (context, state) => const SnackBarScreen(),
+    ),
   ],
 );
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,8 +5,34 @@ class SnackBarScreen extends StatelessWidget {
 
   const SnackBarScreen({super.key});
 
+  void showCustomSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackBar = SnackBar(
+      content: const Text('Hola, soy un SnackBar personalizado'),
+      action: SnackBarAction(
+        label: 'Cerrar',
+        onPressed: (){}
+      ),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SnackBars y Diálogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          showCustomSnackBar(context);
+        },
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+      ),
+    );
   }
 }
